88b0f16b99d993e7be01c85e27ca43564c5de216
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-07-26 19:48:46 -0400

    Creates Card Component

---
+++ react-app/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaBook } from "react-icons/fa";
+
+const Card = ({ courseName }) => {
+    return (
+        <div className="w-60 h-72 bg-white rounded-lg border border-slate-800 transition-transform hover:scale-105 duration-500 ease-in-out">
+            <div className='w-full h-5/6 flex justify-center items-center'>
+                <FaBook size={60} color='gray'/>
+            </div>
+            <div className='h-1/6 bg-slate-100 rounded-b-lg flex justify-center items-center'>
+                <span>{courseName}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
