529e4b3287731a59f04917798af6d31f9ba90b1c
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-07-26 20:13:13 -0400

    Creates Sidebar Component

---
+++ react-app/src/components/sidebar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { LuClock4 } from "react-icons/lu";
+import { MdOutlineLibraryBooks } from "react-icons/md";
+import { FaPeopleGroup } from "react-icons/fa6";
+
+export default function Sidebar() {
+
+    return (
+        <div className='w-96 h-full bg-gray-100'>
+            <div className="flex flex-col w-full text-left ml-11 text-lg space-y-4 mt-11">
+                <div className="flex flex-row items-center text-blue-500 space-x-2">
+                    <LuClock4 />
+                    <span>Recent Courses</span>
+                </div>
+                <div className="flex flex-row items-center text-black space-x-2">
+                    <MdOutlineLibraryBooks />
+                    <span>Courses</span>
+                </div>
+                <div className="flex flex-row items-center text-black space-x-2">
+                    <FaPeopleGroup />
+                    <span>Browse</span>
+                </div>
+            </div>
+        </div>
+    )
+}
