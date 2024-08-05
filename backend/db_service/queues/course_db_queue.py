4ad9ba05c9f1bc32b79428edb99ee0e9041f8e4c
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-08-05 04:27:36 -0400

    Creates course_db_queue in db_service

---
+++ backend/db_service/queues/course_db_queue.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import re
+import json
+
+from firebase_admin import firestore
+
+# adds top-level project directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from db_service.database import get_db
+from common.comms import start_consuming_service, request_service, request_service_with_response
+
+
+def handle_course_db_requests(data):
+    print("Handling course db requests")
+
+    try:
+        if data["action"] == "create_course":
+            course_data = {
+                "course_name": data["course_name"],
+                "course_description": data["course_description"],
+                "course_author_uid": data["uid"],
+                "course_material": data["pdf_material"],
+                "course_topics": data["topics"]
+            }
+            course_ref = course_collection.add(course_data)
+            course_id = course_ref.id
+
+            return {
+                "status": "success",
+                "message": "Course created",
+                "course_id": course_id
+            }
+        
+        elif data["action"] == "add_lessons_to_course":
+            course_id = data["course_id"]
+            lessons = data["lessons"]
+
+            course_ref = course_collection.document(course_id)
+            course_ref.update({"lessons": lessons})
+
+            return {
+                "status": "success",
+                "message": "Lessons added to course"
+            }
+
+        elif data["action"] == "get_course":
+            course_id = data["course_id"]
+            course_ref = course_collection.document(course_id)
+            course_data = course_ref.get()
+
+            return {
+                "status": "success",
+                "message": "Course retrieved",
+                "course_data": course_data.to_dict()
+            }
+        
+        elif data["action"] == "get_courses_by_author":
+            author_uid = data["uid"]
+            courses = course_collection.where("course_author_uid", "==", author_uid).stream()
+
+            course_data = []
+            for course in courses:
+                course_data.append(course.to_dict())
+
+            return {
+                "status": "success",
+                "message": "Courses retrieved",
+                "courses": course_data
+            }
+
+
+    except Exception as e:
+        print(f"Error handling request: {e}")
+        return {
+            "status": "error",
+            "message": f"Error handling request {e}"
+        }
+
+db = get_db()
+
+course_collection = db.collection("courses")
+
+start_consuming_service("course_db", handle_request_func=handle_course_db_requests)
