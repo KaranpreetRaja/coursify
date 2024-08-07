0b74d2d253e79dda45a886008c1a78c5d99a4813
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-08-07 06:03:30 -0600

    Update course_config_main.py

--- backend/course_config_service/course_config_main.py
+++ backend/course_config_service/course_config_main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends
 from routes.create_course import course_create_router
 from routes.get_course import course_get_router
+from routes.chatbot_course import chat_bot_router
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -17,6 +18,7 @@ app.add_middleware(
 # Setup the route for the db-service
 app.include_router(course_create_router, prefix="/api/course_config/create")
 app.include_router(course_get_router, prefix="/api/course_config/get")
+app.include_router(chat_bot_router, prefix="/api/chatbot")
 
 if __name__ == "__main__":
     import uvicorn
