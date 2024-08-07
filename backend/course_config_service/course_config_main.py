f589695a7742093123f511ef8c8b3fe7b2000dd5
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2024-08-07 05:02:16 -0400

    Adds gourse_get_router to course_config_server main script

--- backend/course_config_service/course_config_main.py
+++ backend/course_config_service/course_config_main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Depends
 
 from routes.create_course import course_create_router
+from routes.get_course import course_get_router
 
 app = FastAPI()
 
 # Setup the route for the db-service
 app.include_router(course_create_router, prefix="/api/course_config/create")
+app.include_router(course_get_router, prefix="/api/course_config/get")
 
 if __name__ == "__main__":
     import uvicorn
