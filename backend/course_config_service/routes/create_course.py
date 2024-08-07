04e748ecac72564bcbf90a4a9f0cf6b295c10357
SpaceIbrahim <ibrahimm4566@gmail.com>
2024-08-07 01:37:19 -0400

    Fixed create course to take in form input and generate course

--- backend/course_config_service/routes/create_course.py
+++ backend/course_config_service/routes/create_course.py
@@ -3,6 +3,7 @@ from pypdf import PdfReader
 from typing import List, Optional
 import sys
 import os
+import json
 
 # adds top-level project directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
@@ -131,28 +132,23 @@ Error Body:
 '''
 @course_create_router.post("/set_course_topics")
 async def set_course_topics(
-    uid: str = Form(...)
-    # session_id: str,
-    # course_id: str,
-    # topics: dict
+    uid: str = Form(...),
+    session_id: str = Form(...),
+    course_id: str = Form(...),
+    topics: str = Form(...)
     ):
 
-    course_id = "HEGotQZGd2shO3QIqvul"
-    topics = [
-      { 
-        "topic": "Gradient Estimation and Gradient Descent",
-        "explanation": "This lesson will focus on gradient estimation and gradient descent, which are important concepts in machine learning. It will cover the basics of gradient estimation, including partial derivatives and the chain rule. The lesson will also introduce the concept of gradient descent and explain how it is used to optimize machine learning models. Additionally, it will cover the different types of gradient descent, such as batch gradient descent, stochastic gradient descent, and mini-batch gradient descent. Finally, the lesson will provide examples of how gradient estimation and gradient descent are used in practice to improve the performance of machine learning models." 
-      }
-    ]
     try:
         print("in")
+        topics = json.loads(topics)
+        
         data = {
             "action": "set_topics",
             "uid": uid,
             "course_id": course_id,
             "topics": topics
         }
-        print(topics)
+
         print("sent response")
         response = request_service_with_response("topic_gen", data)
         print("recieved response")
